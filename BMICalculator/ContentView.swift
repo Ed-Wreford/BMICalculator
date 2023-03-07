0196412b7fa466743f2fd95cea1eec074779d3f8
Ed-Wreford <wreford.e@etoncollege.org.uk>
2023-03-07 09:56:26 +0000

    Initial Commit

---
+++ BMICalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BMICalculator
+//
+//  Created by David Cormell on 23/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
